c11a3064c3897c5d92c49710ed36f05b332dae7c
Aine <ainekilbane@gmail.com>
2025-08-21 16:00:06 +0100

    finalised test

--- weather.js
+++ weather.js
@@ -1,9 +1,11 @@
 async function getWeather() {
     const response = await fetch("https://api.weatherapi.com/v1/current.json?q=athlone&key=236b515d0fcb4702a3f114622252108");
+    
     weatherOBJ = await response.json();
+
     console.log(weatherOBJ.current);
     console.log(weatherOBJ.current.temp_c);
-    console.log(weatherOBJ[0]);
-    para = document.getElementById("weather").innerText;
-    para = weatherOBJ.current;
+
+    para = document.getElementById("weather");
+    para.innerText = "the temperature in Athlone is now: " + weatherOBJ.current.temp_c;
 }
