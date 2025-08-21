f3901fdb94571f51acfe72625ad94a5a92a2ea18
Aine <ainekilbane@gmail.com>
2025-08-21 14:42:37 +0100

    chnag4

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 async function getWeather() {
     const response = await fetch("https://api.weatherapi.com/v1/current.json?q=athlone&key=236b515d0fcb4702a3f114622252108");
     weatherOBJ = await response.json();
-    console.log(weatherOBJ[temp_c]);
+    console.log(weatherOBJ[1]);
     para = document.getElementById("weather").innerText;
-    para = weatherOBJ[temp_c];
+    para = weatherOBJ[1].current;
 }
