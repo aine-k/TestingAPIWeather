9d3446c8b6d513899c1e1ec95a6af1984c317b63
Aine <ainekilbane@gmail.com>
2025-08-22 10:49:38 +0100

    request json from local server API

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 async function getWeather() {
-    const response = await fetch("https://api.weatherapi.com/v1/current.json?q=athlone&key=236b515d0fcb4702a3f114622252108");
+    const response = await fetch("localhost:8080/api/hello?");
     
     weatherOBJ = await response.json();
 
